7b1562d52188650aea92e58830d6b77b4f58af09
Josephine <starlightcamera.mail@gmail.com>
2024-02-04 17:52:16 +0800

    frontend done

--- yes-message/src/App.js
+++ yes-message/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import hugBear from "./hug-bear-unscreen.gif";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState(
+        "Hi dear, will you be my valentine?"
+    );
+    const [currentImage, setCurrentImage] = useState(hugBear);
+
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={currentImage} />
+                <h1 className="text-xl font-bold tracking-tight text-gray-900">{message}</h1>
+                <div className="flex m-5">
+                    <button
+                        type="submit"
+                        class="mr-5 w-full items-center justify-center rounded-md border border-transparent bg-green-600 px-6 py-2 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">
+                        Yes
+                    </button>
+                    <button
+                        type="submit"
+                        class="w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-6 py-2 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+                        No
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
